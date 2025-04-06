70b1f741a467b8f7283ba83ba3c8be6dfd5a0413
9Neres <ricardoborgescontact@gmail.com>
2025-04-06 11:06:13 -0300

    mini-projeto de loja POO

---
+++ src/loja/cliente/Cliente.java
@@ -0,0 +1,18 @@
+package loja.cliente;
+
+import loja.Produto;
+
+public class Cliente {
+
+    private String nome;
+    private double saldo;
+
+    public Cliente(String nome){
+        this.nome = nome;
+    }
+
+    public void comprar(Produto produto){
+        System.out.println(nome + " esta tentando comprar " + produto.getNome());
+        produto.vender();
+    }
+}

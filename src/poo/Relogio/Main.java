e9c797a167d202347abd9efc91e9772a6f16cdc7
9Neres <ricardoborgescontact@gmail.com>
2025-04-25 13:27:03 -0300

    not up

--- src/poo/Relogio/Main.java
+++ src/poo/Relogio/Main.java
@@ -4,16 +4,14 @@ public class Main {
     public static void main(String [] args){
 
         var brlClock = new BRLClock();
-
-            brlClock.setSegundos(0);
-            brlClock.setMinutos(10);
-            brlClock.setHoras(15);
+        brlClock.setSegundos(0);
+        brlClock.setMinutos(10);
+        brlClock.setHoras(15);
 
         var usaClock = new USAClock();
-
-            usaClock.setSegundos(0);
-            usaClock.setMinutos(36);
-            usaClock.setHoras(23);
+        usaClock.setSegundos(0);
+        usaClock.setMinutos(36);
+        usaClock.setHoras(23);
 
         System.out.println("BR TIME:\t" + brlClock.getHoras() + "h:" + brlClock.getMinutos() + "m: " + brlClock.getSegundos() + "s");
         System.out.println("US TIME:\t" + usaClock.getHoras() + "h:" + usaClock.getMinutos() + "m: " + usaClock.getSegundos() + "s\t" + usaClock.getPeriodoIndicado());

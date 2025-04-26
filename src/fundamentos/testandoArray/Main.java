e0195098294168a5fc904cf0170c7b0e4efba4b4
9Neres <ricardoborgescontact@gmail.com>
2025-04-26 16:35:07 -0300

    add comment

--- src/fundamentos/testandoArray/Main.java
+++ src/fundamentos/testandoArray/Main.java
@@ -6,6 +6,9 @@ import java.util.function.Function;
 
 public class Main {
     public static void main ( String [] args){
+
+        // Arrays, interface e Lambda
+
         List<User> users = List.of(new User("João", 21), new User("Ana", 12), new User("Caua", 18), new User("Vasquez", 29));
 
         List<Account> accounts = List.of(new Account(123, "Paulin@gmail.com\t", "vasco123"), new Account( 721, "lucas.martins82@emailtest.com\t", "filha123"));
@@ -22,15 +25,15 @@ public class Main {
         System.out.println("\n");
 
         accounts.forEach(( Account account ) ->{
-            System.out.println(account);  // forma simplifica de fazer o lambida
+            System.out.println(account);  // forma simplifica de fazer o lambda
         });
 
         System.out.println("\n");
 
-        printStringValue(Record :: toString , notas);
+        printStringValue(Record :: toString , notas); // outra forma também
     }
 
     private static void printStringValue(Function<Nota, String> callback, List<Nota> notas ){
-        notas.forEach( u -> System.out.println(callback.apply(u)));
+        notas.forEach( u -> System.out.println(callback.apply(u))); // estudar como fazer com int ou double
     }
 }

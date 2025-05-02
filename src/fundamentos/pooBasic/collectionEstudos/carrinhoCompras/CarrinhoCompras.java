247ad74227ece41b93df6cee2d0c5d0433543bd4
9Neres <ricardoborgescontact@gmail.com>
2025-05-02 16:00:33 -0300

    carrinho de compras

--- src/fundamentos/pooBasic/collectionEstudos/carrinhoCompras/CarrinhoCompras.java
+++ src/fundamentos/pooBasic/collectionEstudos/carrinhoCompras/CarrinhoCompras.java
@@ -58,18 +58,4 @@ public class CarrinhoCompras {
         return "CarrinhoCompras: " +
                 "itemList=" + itemList;
     }
-
-    public static void main(String[] args) throws Exception {
-
-        CarrinhoCompras carrinhoCompras = new CarrinhoCompras();
-
-        carrinhoCompras.adicionarItem("Biscoito Oreo", 10d, 2);
-        carrinhoCompras.adicionarItem("Arroz", 35d, 1);
-        carrinhoCompras.adicionarItem("feijao", 15d, 2);
-
-        carrinhoCompras.exibirItens();
-        carrinhoCompras.exibirItens();
-
-        System.out.println("" + carrinhoCompras.calcularItens());
-    }
 }
--- src/fundamentos/pooBasic/collectionEstudos/carrinhoCompras/Main.java
+++ src/fundamentos/pooBasic/collectionEstudos/carrinhoCompras/Main.java
@@ -1,4 +1,17 @@
 package fundamentos.pooBasic.collectionEstudos.carrinhoCompras;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+
+        CarrinhoCompras carrinhoCompras = new CarrinhoCompras();
+
+        carrinhoCompras.adicionarItem("Biscoito Oreo", 10d, 2);
+        carrinhoCompras.adicionarItem("Arroz", 35d, 1);
+        carrinhoCompras.adicionarItem("feijao", 15d, 2);
+
+        carrinhoCompras.exibirItens();
+        carrinhoCompras.exibirItens();
+
+        System.out.println("" + carrinhoCompras.calcularItens());
+    }
 }
